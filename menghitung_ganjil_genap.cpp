9c7cde4e923c162d89f734e43fc77e1548e0ad24
TAUPIK <ovikbinasehat@gmail.com>
2017-03-27 23:15:31 +0700

    Add files via upload

---
+++ menghitung_ganjil_genap.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+	int main()	{
+int bilangan;
+	cout <<"masukan bilangan : "<<endl;
+	cin >>bilangan;
+if (bilangan%2==0)
+cout <<"bilangan genap"<<endl;
+else 
+cout <<"bilangan ganjil";
+return 0;
+}
---
+++ menghitung_uisa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int usia;
+cout<<"berapa usia anda ??";
+cin>>usia;
+if (usia <17)
+cout <<"anda belum dewas "<<endl;
+else 
+cout <<"anda dewasa"<<endl;
+return 0;
+}
